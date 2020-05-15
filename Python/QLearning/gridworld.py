a7d0fcd3de263e3d32740414edf3c848a3f23b36
eddyhom <eddy_hom@hotmail.com>
2020-05-15 21:42:38 +0200

    Fixed render function

--- Python/QLearning/gridworld.py
+++ Python/QLearning/gridworld.py
@@ -1,30 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pygame
+import math
 
 import os
 from time import sleep
 
-width = 640
+width = 600
 
 
 class GridWorld(object):
     def __init__(self, m, n):
-        self.grid = np.zeros((m,n))
+        self.grid = np.zeros((m, n))
         self.m = m
         self.n = n
         self.stateSpace = [i for i in range(self.m*self.n)]
-        self.stateSpace.remove(80)
+        self.stateSpace.remove((self.m*self.n) - 1)
         self.stateSpacePlus = [i for i in range(self.m*self.n)]
         self.actionSpace = {'U': -self.m, 'D': self.m,
                             'L': -1, 'R': 1}
-        self.possibleActions = ['U', 'D', 'L', 'R']
+        '''UL': -self.m-1,  'UR': -self.m+1, 'DL': self.m-1, 'DR': self.m+1}'''
+        self.possibleActions = ['U', 'D', 'L', 'R']#, 'UL', 'UR', 'DL', 'DR']
         # dict with magic squares and resulting squares
         self.agentPosition = 0
 
     def isTerminalState(self, state):
         return state in self.stateSpacePlus and state not in self.stateSpace
-
 
     def getAgentRowAndColumn(self):
         x = self.agentPosition // self.m
@@ -43,7 +44,7 @@ class GridWorld(object):
         if newState not in self.stateSpacePlus:
             return True
         # if we're trying to wrap around to next row
-        elif oldState % self.m == 0 and newState  % self.m == self.m - 1:
+        elif oldState % self.m == 0 and newState % self.m == self.m - 1:
             return True
         elif oldState % self.m == self.m - 1 and newState % self.m == 0:
             return True
@@ -51,7 +52,6 @@ class GridWorld(object):
             return False
 
     def step(self, action):
-        agentX, agentY = self.getAgentRowAndColumn()
         resultingState = self.agentPosition + self.actionSpace[action]
 
         reward = -1 if not self.isTerminalState(resultingState) else 0
@@ -65,13 +65,16 @@ class GridWorld(object):
 
     def reset(self):
         self.agentPosition = 0
-        self.grid = np.zeros((self.m,self.n))
+        self.grid = np.zeros((self.m, self.n))
+        self.grid[-1][-1] = 2
         return self.agentPosition
 
     def render(self):
         x, y = 0, 0  # starting position
-        w = 80  # width of each cell
-        border = 78
+        w = width / self.m  # width of each cell
+        border = w - 2
+        robot_size = border / 3
+        robot_pos = w/2 - robot_size / 2
 
         for row in self.grid:
             for col in row:
@@ -79,53 +82,27 @@ class GridWorld(object):
                     pygame.draw.rect(win, (255, 255, 255), (x + 1, y + 1, border, border))
                 elif col == 1:
                     pygame.draw.rect(win, (255, 255, 255), (x + 1, y + 1, border, border))
-                    pygame.draw.rect(win, (250, 0, 0), (x + 20, y + 20, border - 38, border - 38))
+                    pygame.draw.rect(win, (250, 0, 0), (x + robot_pos, y + robot_pos, robot_size, robot_size))
                 elif col == 2:
                     pygame.draw.rect(win, (0, 128, 0), (x + 1, y + 1, border, border))
-                elif col == 3:
-                    pygame.draw.rect(win, (0, 128, 0), (x + 1, y + 1, border, border))
-                elif col == 4:
-                    pygame.draw.rect(win, (0, 0, 255), (x + 1, y + 1, border, border))
-                elif col == 5:
-                    pygame.draw.rect(win, (0, 0, 255), (x + 1, y + 1, border, border))
 
                 pygame.display.update()
                 x = x + w  # move right
             y = y + w  # move down
             x = 0  # rest to left edge
 
-        '''
-        print('------------------------------------------')
-        for row in self.grid:
-            for col in row:
-                if col == 0:
-                    print('-', end='\t')
-                elif col == 1:
-                    print('X', end='\t')
-                elif col == 2:
-                    print('Ain', end='\t')
-                elif col == 3:
-                    print('Aout', end='\t')
-                elif col == 4:
-                    print('Bin', end='\t')
-                elif col == 5:
-                    print('Bout', end='\t')
-            print('\n')
-        print('------------------------------------------')
-        sleep(0.3)
-        os.system('cls')
-    '''
     def actionSpaceSample(self):
         return np.random.choice(self.possibleActions)
 
 def maxAction(Q, state, actions):
-    values = np.array([Q[state,a] for a in actions])
+    values = np.array([Q[state, a] for a in actions])
     action = np.argmax(values)
     return actions[action]
 
 if __name__ == '__main__':
     # map magic squares to their connecting square
-    env = GridWorld(9, 9)
+    grids = 11
+    env = GridWorld(grids, grids)
     # model hyperparameters
     ALPHA = 0.1
     GAMMA = 1.0
@@ -139,46 +116,48 @@ if __name__ == '__main__':
     numGames = 50000
     totalRewards = np.zeros(numGames)
     rend = False
-    pygame.init()
-    win = pygame.display.set_mode((width, width))
-    pygame.display.set_caption("First Game")
 
     for i in range(numGames):
+        print('starting game ', i)
         if (i+1) % 5000 == 0:
             print('starting game ', i)
             rend = True
+            pygame.init()
+            win = pygame.display.set_mode((width, width))
+            pygame.display.set_caption("First Game")
         else:
             rend = False
+            pygame.quit()
 
         done = False
         epRewards = 0
         observation = env.reset()
         while not done:
             rand = np.random.random()
-            action = maxAction(Q,observation, env.possibleActions) if rand < (1-EPS) \
+            action = maxAction(Q, observation, env.possibleActions) if rand < (1-EPS) \
                                                     else env.actionSpaceSample()
             observation_, reward, done, info = env.step(action)
             epRewards += reward
+            '''if epRewards < -2000:
+                epRewards = -3000
+                done = True'''
 
             action_ = maxAction(Q, observation_, env.possibleActions)
-            Q[observation,action] = Q[observation,action] + ALPHA*(reward + \
-                        GAMMA*Q[observation_,action_] - Q[observation,action])
+            Q[observation, action] = Q[observation, action] + ALPHA*(reward + \
+                        GAMMA*Q[observation_, action_] - Q[observation, action])
             observation = observation_
 
-            if(rend):
-                pygame.time.delay(100)
+            if rend:
+                pygame.time.delay(50)
                 env.render()
 
-
-
-
         if EPS - 2 / numGames > 0:
+            #EPS = math.sqrt(EPS)
             EPS -= 2 / numGames
         else:
             EPS = 0
         totalRewards[i] = epRewards
+        print(epRewards)
 
     plt.plot(totalRewards)
     plt.show()
-
-pygame.quit()
