04537808c0ae083db3080cfac3dd7b0b5da3dd80
eddyhom <eddy_hom@hotmail.com>
2020-06-10 14:46:35 +0200

    All fixed, except Render

--- Python/QLearning/Gridworld.py
+++ Python/QLearning/Gridworld.py
@@ -13,23 +13,20 @@ class GridWorld(object):
     def __init__(self, w, h): #m and n, shape of the grid
         self.w = w  # Width
         self.h = h  # Height
+        self.direction = 'E' #Different directions are East 'E', West 'W', North 'N', South 'S'
 
         self.agentPosition = [0, 0]  # Robots position
         self.goal = [self.w, self.h] # Positive Person
 
         states = []
-        state = []
         for i in range(self.w+1): #Still working on this!!!!!!
             for j in range(self.h+1):
                 dx = self.goal[0] - i
                 dy = self.goal[1] - j
-                angle = ''
-                if i > j:
-                    angle = 'w'
-                else:
-                    angle = 'h'
-
-                states.append((format(sqrt(dx**2 + dy**2), '.2f'), angle, (i,j)))   # State space distance to goal (for now every meter)
+                for quadrant in range(0, 4):
+                    dist = round(sqrt(dx**2 + dy**2), 2)
+                    if (dist, quadrant) not in states:
+                        states.append((dist, quadrant))   # State space distance to goal (for now every meter)
 
         self.stateSpace = states
 
@@ -39,38 +36,79 @@ class GridWorld(object):
 
 
     def isTerminalState(self, state):
-        return state == self.goal
+        #print(state)
+        #print("Here im taking away 0: ", state[0]-0)
+        if abs(state[0]-0) <= 0.01:
+         #   print(True)
+            return True
+        else:
+          #  print(False)
+            return False
 
-    def setState(self, state): #Takes the new state as input
-        self.agentPosition = state #Agent position is the new state
 
-    def offGridMove(self, newState, oldState): #Change input if needed
-        # if newState outside return True
-        # else if newState within boundaries return False
-
-
-    def getState(self, action):
-        resultingPosition = [self.agentPosition[0] + self.actionSpace[action][0],
+    def setAgentPosition(self, action): #Takes the new action as input
+        self.agentPosition = [self.agentPosition[0] + self.actionSpace[action][0],
                           self.agentPosition[1] + self.actionSpace[action][1]]
-        #newState =
+
+    def offGridMove(self, action): #Change input if needed
+        position = [self.agentPosition[0] + self.actionSpace[action][0],
+                          self.agentPosition[1] + self.actionSpace[action][1]]
+
+        return not (0 <= position[0] <= self.w and 0 <= position[1] <= self.h)
+
+    def getQuadrant(self, newPosition):
+        pos = [self.goal[0] - newPosition[0], self.goal[1] - newPosition[1]]
+
+        if pos[0] >= 0:
+            if pos[1] >= 0:
+                return 3
+            else:
+                return 0
+        else:
+            if pos[1] >= 0:
+                return 2
+            else:
+                return 1 #Returns in what quadrant the goal is with respect to the robot
+
+    def getDist(self, position):
+        dx = self.goal[0] - position[0]
+        dy = self.goal[1] - position[1]
+
+        return round(sqrt(dx ** 2 + dy ** 2), 2)
+
+    def getState(self, newState):
+        resultingPosition = [self.agentPosition[0] + self.actionSpace[newState][0],
+                          self.agentPosition[1] + self.actionSpace[newState][1]]
+
+        return tuple([self.getDist(resultingPosition), self.getQuadrant(resultingPosition)])
+
+    def giveReward(self, newState):
+        if not self.isTerminalState(newState):
+            currentDist = self.getDist(self.agentPosition)
+            if newState[0] < currentDist:
+                return -1
+            else:
+                return -5
+        else:
+            return 0
 
     def step(self, action):
-        resultingState = [self.agentPosition[0] + self.actionSpace[action][0],
-                          self.agentPosition[1] + self.actionSpace[action][1]]
+        resultingState = self.getState(action)
 
-        reward = -1 if not self.isTerminalState(resultingState) else 0
+        reward = self.giveReward(resultingState)
 
-        if not self.offGridMove(resultingState, self.agentPosition): #If not moving off grid
-            self.setState(resultingState)
+        if not self.offGridMove(action): #If not moving off grid
+            self.setAgentPosition(action)
             return resultingState, reward, \
                    self.isTerminalState(resultingState), None
         else:
-            return self.agentPosition, reward, \
-                   self.isTerminalState(self.agentPosition), None
+            return tuple([self.getDist(self.agentPosition), self.getQuadrant(self.agentPosition)]), \
+                   reward, \
+                   self.isTerminalState([self.getDist(self.agentPosition), self.getQuadrant(self.agentPosition)]), None
 
     def reset(self):
-        # Reset what must be reset and
-        # Return Initial state (distance to goal and angle)
+        self.agentPosition = [0, 0]  # Robots position
+        return tuple([self.getDist([0, 0]), self.getQuadrant([0, 0])])
 
     def render(self):
         x, y = 0, 0  # starting position
@@ -104,9 +142,10 @@ def maxAction(Q, state, actions):
     action = np.argmax(values)
     return actions[action]
 
-'''def checkIfDuplicates_1(routes):
+def checkIfDuplicates_1(routes):
     dups = {tuple(x) for x in routes if routes.count(x) > 1}
-    print(dups)'''
+    print(dups)
+    print(len(dups))
 
 if __name__ == '__main__':
     # map magic squares to their connecting square
@@ -118,17 +157,14 @@ if __name__ == '__main__':
     discount = 1.0 #Determines the importance of future rewards.
     EPS = 1.0
 
-    print(env.stateSpace)
-    print(len(env.stateSpace))
-
-    #checkIfDuplicates_1(env.stateSpace)
-
-
     Q = {}
     #Want to find a value of state action pairs
     for state in env.stateSpace:
         for action in env.possibleActions:
             Q[state, action] = 0
+
+    print(Q)
+    print(len(Q))
 
 
     numGames = 50000
@@ -137,15 +173,15 @@ if __name__ == '__main__':
 
     for i in range(numGames):
         print('starting game ', i)
-        if i % 5000 == 0:
+        if i % 100 == 0:
             print('starting game ', i)
-            rend = True
-            pygame.init()
-            win = pygame.display.set_mode((env.w, env.h))
-            pygame.display.set_caption("First Game")
-        else:
-            rend = False
-            pygame.quit()
+            #rend = True
+        #    pygame.init()
+        #    win = pygame.display.set_mode((env.w, env.h))
+        #    pygame.display.set_caption("First Game")
+        #else:
+            #rend = False
+            #pygame.quit()
 
         done = False
         epRewards = 0
@@ -160,6 +196,9 @@ if __name__ == '__main__':
             observation_, reward, done, info = env.step(action)
             epRewards += reward
 
+            if env.agentPosition == env.goal:
+                print("Agent position :", env.agentPosition, "and Done is: ", done)
+
             action_ = maxAction(Q, observation_, env.possibleActions)
 
             #Update Q-Table with new knowledge
@@ -167,9 +206,9 @@ if __name__ == '__main__':
                         discount*Q[observation_, action_] - Q[observation, action])
             observation = observation_ #Environment has changed state (state = new_state)
 
-            if rend:
-                pygame.time.delay(50)
-                env.render()
+            #if rend:
+                #pygame.time.delay(50)
+                #env.render()
 
         if EPS - 2 / numGames > 0:
             #EPS = math.sqrt(EPS)
