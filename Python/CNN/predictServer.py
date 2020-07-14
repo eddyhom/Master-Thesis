65e981adfa2b6ee41a48860a9ab0db71ac6c46cd
eddyhom <eddy_hom@hotmail.com>
2020-07-14 20:00:17 +0200

    Update predictServer.py
    Working Server

--- Python/CNN/predictServer.py
+++ Python/CNN/predictServer.py
@@ -1,24 +1,22 @@
 import tensorflow as tf
 import cv2
 import numpy as np
-import time
-
-predEmo = ["anger_disgust_fear_sadness", "happy_surprise"]
+from math import floor
 
 
-def predict(model):
-    imgname = "emotion.png"
+predEmo = ["negative", "positive"]
+size = 2
+IMG_SIZE = 48
+NEW_SIZE = (IMG_SIZE, IMG_SIZE)
 
-    im = cv2.imread(imgname, cv2.IMREAD_GRAYSCALE)
-    im = tf.cast(im, tf.float32)
+
+def predict(model, face):
+    im = cv2.resize(face, NEW_SIZE) / 255
+
     im = (np.expand_dims(im, 2))
     im = (np.expand_dims(im, 0))
 
-    t1 = time.time()
     prediction = model.predict(x=im)  # NEGATIVE = -X, POSITIVE = X.
-    t2 = time.time()
-
-    print("Time taken was: ", t2-t1)
 
     pred = prediction[0]
     max_val = max(pred)
@@ -27,6 +25,26 @@ def predict(model):
 
     return predEmo[i[0]]
 
+
+def isFace(face_cascade, model, pic_path):
+    img_name = pic_path
+    im = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE)
+
+    if im is not None: #If image was open succesfully
+        # Resize the image to speed up detection
+        im = cv2.resize(im, (int(im.shape[1] / size), int(im.shape[0] / size)))
+        faces = face_cascade.detectMultiScale(im, 1.3, 5)
+        if len(faces) > 0:
+            (x, y, w, h) = faces[0]
+            face = im[y*4:y*4 + h*4, floor(x*4+(x+4)*0.08):floor(4*(x+w) - 4*(x+w)*0.08)]
+
+            emo = predict(model, face)
+            return 1 if emo == predEmo[1] else -1 #Return 1 if positive -1 if negative
+        else:
+            return 0
+    else:
+        print("image: ", img_name, " couldnt be read")
+        return 0
 
 
 if __name__ == '__main__':
